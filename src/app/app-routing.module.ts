f3752c1a2efb0bea7aede375f1c42c57b83c048b
Eliud Juárez <juarez_s3@hotmail.com>
2022-01-12 00:45:53 -0600

    CMS Module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -51,6 +51,10 @@ const routes: Routes = [
     ]
   },
   {
+    path: 'cms',
+    loadChildren: () => import('./cms/cms.module').then(m => m.CmsModule)
+  },
+  {
     path: '**',
     component: NotFoundComponent 
   }
